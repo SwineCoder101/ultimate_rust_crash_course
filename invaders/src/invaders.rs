6871527452993e94eb291803bf5b645ef146baa0
Liam Faruq <liam@Liams-MacBook-Pro.local>
2024-06-21 17:43:44 +0100

    add-files

---
+++ invaders/src/invaders.rs
@@ -0,0 +1,36 @@
+use rusty_time::timer::Timer;
+use crate::{NUM_COLS, NUM_ROWS};
+
+pub struct Invader {
+    x: usize,
+    y: usize
+}
+
+pub struct Invaders {
+    pub army: Vec<Invader>,
+    move_timer: Timer,
+    direction: i32,
+}
+
+impl Invaders {
+    pub fn new() -> Self {
+        let mut army = Vec::new();
+        for x in 0..NUM_COLS {
+            for y in 0..NUM_ROWS{
+                if ( x > 1)
+                    && ( x < NUM_COLS - 2)
+                    && (y > 0)
+                    && ( y < 9)
+                    && (x % 2 == 0)
+                    && (y % 2 ==0 ){
+                    army.push(Invader {x,y});
+                }
+            }
+        }
+        Self{
+            army,
+            move_timer: Timer::from_millis(2000),
+            direction:1,
+        }
+    }
+}
--- invaders/src/lib.rs
+++ invaders/src/lib.rs
@@ -3,6 +3,7 @@ pub mod render;
 pub mod player;
 pub mod shot;
 
+pub mod invaders;
 
 pub const NUM_ROWS: usize = 25;
 pub const NUM_COLS: usize = 80;
